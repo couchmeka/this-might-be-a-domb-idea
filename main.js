1deb0f98e38c3b4caaf42787394783b64e86dc6b
CouchMeka <meka.daniels1@gmail.com>
2022-10-24 15:31:01 -0400

    wont get fooled

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+//paragraph
+let paragraph = document.querySelector('p');
+paragraph.style.background = "#ADD8E6";
+paragraph.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
+
+//Item 13
+let item13 = document.querySelector('#item-13')
+item13.style.opacity =.5;
+
+
+//Heading
+let heading = document.querySelector('head');
+heading.style.fontSize = "48px";
+
+//Item 3
+let item3 =document.querySelector('#item-3');
+item3.innerText = " I say, Hi!"
+
+//First Image
+
+let image = document.querySelector('img');
+image.src ="https://thumbs.dreamstime.com/b/laguna-beach-coast-18074672.jpg"
+image.height = "300"
+
+//Second Image
+let image2 = document.querySelector('#image')
+image2.height ="300"
+
+//16
+let item16 = document.querySelector('#item-16')
+item16.className = "item";
+item16.innerText = "Won't get fooled again"

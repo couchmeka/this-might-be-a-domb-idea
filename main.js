cb9de7729a20eb85733a1b73bd017ac26b95267b
CouchMeka <meka.daniels1@gmail.com>
2022-10-25 10:56:14 -0400

    updated the background color to font color

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 //paragraph
 let paragraph = document.querySelector('p');
-paragraph.style.background = "#ADD8E6";
+paragraph.style.color = "#ADD8E6";
 paragraph.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
 
 //Item 13
@@ -19,7 +19,6 @@ let item3 =document.querySelector('#item-3');
 item3.innerText = " I say, Hi!"
 
 //First Image
-
 let image = document.querySelector('img');
 image.src ="https://thumbs.dreamstime.com/b/laguna-beach-coast-18074672.jpg"
 image.height = "300"
